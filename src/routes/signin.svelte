6ac4f54efbe3c4448cdff29ef71b951c5049f47c
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-14 10:56:04 +0300

    add sign in page

---
+++ src/routes/signin.svelte
@@ -0,0 +1,17 @@
+<div class="container">
+	<h1>Sign In</h1>
+	<form>
+		<div class="form-controls">
+			<label for="email">Email:</label>
+			<input type="email" name="email" id="email" />
+		</div>
+		<div class="form-controls">
+			<label for="password">Password:</label>
+			<input type="password" name="password" id="password" />
+		</div>
+		<div class="form-controls">
+			<button type="submit">Sign In</button>
+			<a href="/sign-up">Sign Up</a>
+		</div>
+	</form>
+</div>
