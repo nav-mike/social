cc0508554cee295f3bb0045b263ad00d3574e989
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-16 12:16:14 +0300

    add user card component

---
+++ src/lib/UserCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let avatarUrl: string;
+	export let name: string;
+	export let username: string;
+	export let isReal: boolean = false;
+	export let id: string;
+</script>
+
+<div>{name}</div>
