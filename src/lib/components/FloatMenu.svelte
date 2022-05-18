737d08579e30c2bfcde143288da0119fde1db9d9
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-18 18:26:04 +0300

    add float menu component

---
+++ src/lib/components/FloatMenu.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let state: 'hidden' | 'visible' = 'hidden';
+</script>
+
+<div class={state}>
+	<slot />
+</div>
+
+<style>
+	.hidden {
+		display: none;
+	}
+
+	.visible {
+		position: absolute;
+		background-color: black;
+		box-shadow: rgb(255 255 255 / 20%) 0px 0px 15px, rgb(255 255 255 / 15%) 0px 0px 3px 1px;
+
+		display: flex;
+		flex-direction: column;
+		border-radius: 5px;
+	}
+</style>
