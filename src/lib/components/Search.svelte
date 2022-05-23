769c9703f6e20b39184ef6a6f9704552746219dc
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-05-23 18:12:58 +0300

    make search more twitter-like

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,11 +1,29 @@
-<div>
-	<i class="fab fa-searchengin" />
-	<input type="text" placeholder="Search Twitter" />
+<script lang="ts">
+	let active: boolean = false;
+
+	const activeSearchHandler = () => {
+		active = true;
+	};
+
+	const inactiveSearchHandler = () => {
+		active = false;
+	};
+</script>
+
+<div class:active>
+	<i class="fab fa-searchengin" class:active />
+	<input
+		type="text"
+		placeholder="Search Twitter"
+		on:focus={activeSearchHandler}
+		on:focusout={inactiveSearchHandler}
+	/>
 </div>
 
 <style>
 	div {
 		background-color: rgb(47, 51, 54);
+		border: rgb(47, 51, 54) 1px solid;
 		display: flex;
 		align-items: center;
 		justify-content: space-evenly;
@@ -15,8 +33,16 @@
 		border-radius: 40px;
 	}
 
+	div.active {
+		border: rgb(29, 155, 240) solid 1px;
+	}
+
 	i {
 		color: grey;
+	}
+
+	i.active {
+		color: rgb(29, 155, 240);
 	}
 
 	input {
@@ -24,8 +50,12 @@
 		border: none;
 		background-color: transparent;
 		padding: 10px;
+		color: white;
 	}
 
-	input:active {
+	input:active,
+	input:focus {
+		border: none;
+		outline: none;
 	}
 </style>
